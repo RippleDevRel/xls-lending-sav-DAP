6316470baf0e32f145a8db4fa890995c16bac16a
maximedgr <107214125+maximedgr@users.noreply.github.com>
2026-05-27 14:08:59 +0100

    Remove homegrown auth module and old Session model

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,5 +1,4 @@
 export { connectDB } from "./connection";
-export { SessionModel } from "./models/session";
 export { UserWalletsModel } from "./models/user-wallets";
 export { VaultModel } from "./models/vault";
 export { LoanModel } from "./models/loan";
