bc84ce906b805a4836c73d992d4f9193d49071c6
maximedgr <107214125+maximedgr@users.noreply.github.com>
2026-05-27 13:52:57 +0100

    Add UserWallets Mongoose model keyed by auth0Sub

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,5 +1,6 @@
 export { connectDB } from "./connection";
 export { SessionModel } from "./models/session";
+export { UserWalletsModel } from "./models/user-wallets";
 export { VaultModel } from "./models/vault";
 export { LoanModel } from "./models/loan";
 export { DepositHistoryModel } from "./models/deposit-history";
